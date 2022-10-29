91d598695a0416ae83a3951ef93b64fae9308583
error-rgl <rglcnd@gmail.com>
2022-10-28 23:24:10 -0500

    [Feature]: Add new form to the Log in

--- hbtn-frontend-v2/src/pages/login/Login.jsx
+++ hbtn-frontend-v2/src/pages/login/Login.jsx
@@ -3,14 +3,15 @@ import { useEffect, useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './login.css'
 import { useAuth } from "@/hooks";
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from 'react-google-login';
 export function Login() {
   const navigate = useNavigate()
   const { login, isLogged, loading } = useAuth()
 
   useEffect(() => {
-    // isLogged && navigate('/')
-  }, [])
+    isLogged && navigate('/')
+  }, [isLogged])
 
   async function handleCallbackResponse(response) {
     const token = response.credential;
@@ -20,44 +21,42 @@ export function Login() {
   useLayoutEffect(() => {
     google.accounts.id.initialize({
       client_id: "422076817865-9dbp6oce8lv11muqibebec3lusskrb6t.apps.googleusercontent.com",
-      // render:(renderProps =>{
-      //   <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Inicia session</button>
-      // }),
       callback: handleCallbackResponse
     });
     google.accounts.id.renderButton(
       document.getElementById("signInDiv"),
-      { theme: "outline", width: 300 }
+      { theme: "outline", width: 283 }
     );
   }, []);
 
   return (
     <div className="App">
-      <form class="f-login">
+      <form className="f-login">
 
-        <div class="segment tittle">
+        <div className="segment tittle">
           <h1>Iniciar Sesion</h1>
         </div>
-        <div class="logo">
+        <div className="logo1">
           <img src="https://holberton.anahuac.mx/wp-content/uploads/Group-359.png"></img>
         </div>
-        <GoogleLogin
+        <div id="signInDiv"></div>
+        {/* <GoogleLogin
         clientId="422076817865-9dbp6oce8lv11muqibebec3lusskrb6t.apps.googleusercontent.com"
         render={renderProps =>(
-              <button class="red mybtn act" type="button" id="signInDiv" onClick={renderProps.onClick} disabled={renderProps.disabled}>Iniciar con google</button>
+              <button className="red mybtn act" type="button" id="signInDiv" onClick={renderProps.onClick} disabled={renderProps.disabled}>Iniciar con google</button>
           )}
           cookiePolicy={'single_host_origin'}
-        />
-        {/* <button class="red mybtn act" type="button" id="signInDiv"> Iniciar con google</button> */}
+        onSuccess={handleCallbackResponse}
+        /> */}
+        {/* <button className="red mybtn act" type="button" id="signInDiv"> Iniciar con google</button> */}
         
-        <div class="segment">
-          <button class="unit mybtn" type="button"></button>
-          <button class="unit mybtn" type="button"></button>
-          <button class="unit mybtn" type="button"></button>
+        <div className="segment">
+          <button className="unit mybtn" type="button"></button>
+          <button className="unit mybtn" type="button"></button>
+          <button className="unit mybtn" type="button"></button>
         </div>
         
       </form>
-      {/* <div id="signInDiv"></div> */}
 
   </div>
   );
